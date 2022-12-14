49083ac99a62985534153a8cdae8cd3fcc8dded2
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2022-12-14 09:18:17 -0500

    Added Home.jsx, Login.jsx, altered login info

--- src/App.jsx
+++ src/App.jsx
@@ -2,65 +2,27 @@ import { useState } from 'react'
 import './App.scss'
 import './setUp.scss'
 import './Components/Fontawesome'
-import firebase from './Components/firebase'
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
+import LogIn from './Components/Login';
+import { BrowserRouter, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
-function App() {
-  
-  const auth = getAuth(firebase);
 
-  const signUp = () => {
-
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      console.log(user)
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      // const errorMessage = error.message;
-      alert(errorCode)
-      // ..
-    });
-  }
-  
+function App() {  
 
   return (
- <>
- <body>
-  
- <div className="wrapper">
  
-  <form action="">
-    <div className="container">
-      <div className="input">
-        <FontAwesomeIcon icon="fa-solid fa-envelope" />
-        <input type={"email"}  onChange={(event) => event.target.value} placeholder="Email" />
-      </div>
-      <div className="input">
-        <FontAwesomeIcon icon="fa-solid fa-lock" />
-        <input type="text"  onChange={(event) => event.target.value} placeholder="Password"/>
-      </div>
-      <div className="input"> 
-        <FontAwesomeIcon icon="fa-solid fa-key" />     
-        <input type="text" onChange={(event) => event.target.value} placeholder="Confirm password" />
-      </div>
+<>
+<BrowserRouter>
+<Routes>
+ <Route path='/' element={<LogIn/>}/>
 
-    <button onClick={signUp}>Sign Up</button>
-    <button>Sign In</button>
+</Routes>
 
-    </div>
-  </form>
-
-  
- </div>
- </body>
- </>
+</BrowserRouter>
+</>
+ 
+ 
   )
 }
 
---
+++ src/Components/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import './Fontawesome'
+import firebase from './firebase'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
+
+const LogIn = () => {
+    const auth = getAuth(firebase);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState([])
+
+    const signUp = () => {
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+    setUser(userCredential.user);
+    console.log(user)
+    
+      // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+      // const errorMessage = error.message;
+        alert(errorCode)
+      // ..
+    });
+    }
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+         <div className="wrapper">
+ 
+  <form action="" onSubmit={handleOnSubmit}>
+  <span className="daily-planner"><h3>Daily Planner</h3></span>
+    <div className="container">
+      <div className="input">
+        <FontAwesomeIcon icon="fa-solid fa-envelope" />
+        <input type={"email"}  onChange={(event) => setEmail(event.target.value)} placeholder="Email" />
+        <span className="underline"></span>
+
+      </div>
+      <div className="input">
+        <FontAwesomeIcon icon="fa-solid fa-lock" />
+        <input type="text"  onChange={(event) => setPassword(event.target.value)} placeholder="Password"/>
+        <span className="underline"></span>
+
+      </div>
+      <div className="input"> 
+        <FontAwesomeIcon icon="fa-solid fa-lock" />     
+        <input type="text" onChange={(event) => event.target.value} placeholder="Confirm password" />
+        <span className="underline"></span>
+
+      </div>
+      <div className="button">
+
+        <button onClick={signUp}>Sign Up</button>
+        <button>Sign In</button>
+      </div>
+
+
+    </div>
+  </form>
+
+  
+ </div>
+    )
+}
+
+export default LogIn
