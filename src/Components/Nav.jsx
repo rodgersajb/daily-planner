69314a5993f12d5a3adcbc5faff1b5593cb1c86b
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-01-15 14:33:17 -0500

    Moved Home Component into Nav Component, added floating dropdown menu onClick

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -14,6 +14,11 @@ import Notes from "./Notes";
 const Nav = () => {
   const [date, setDate] = useState([]);
   const { currentUser, setCurrentUser } = useContext(AuthContext);
+  const [priorities, setPriorities] = useState([]);
+  const [reminders, setReminders] = useState([]);
+  const [todo, setTodo] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [open, setOpen] = useState(false);
 
   console.log(currentUser, "current user");
 
@@ -33,33 +38,53 @@ const Nav = () => {
     <Navigate to="/" />
   ) : (
     <>
-      <div className="nav-wrapper">
-        <nav>
-          <div className="plus">
-            <FontAwesomeIcon icon="fa-solid fa-plus" />
-          </div>
-
-          <h1>Daily Planner</h1>
-
-          <p className="date">{date.toLocaleString()}</p>
-          <div className="dropdown-menu">
-            <FontAwesomeIcon icon="fa-solid fa-user" />
-            <div className="dropdown-content">
-              <p>Logged in as {currentUser.email}</p>
-              <button onClick={handleSignOut}>
-                <FontAwesomeIcon icon="fa-solid fa-right-from-bracket" />
-                <p>Sign Out</p>
-              </button>
+      <section className="nav">
+        <div className="nav-wrapper">
+          <nav>
+            <div className="plus-container">
+              <div
+                className="plus-trigger"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              >
+                <FontAwesomeIcon icon="fa-solid fa-plus" />
+              </div>
             </div>
+            <div className={`plus-dropdown ${open ? "active" : "inactive"}`}>
+              <label htmlFor=""></label>
+              <input type="text" />
+              <select name="" id="">
+                <option value="">Add to...</option>
+                <option value="priorities">Top Priorities{priorities}</option>
+                <option value="">To Do{todo}</option>
+                <option value="">Reminders{reminders}</option>
+                <option value="">Notes{notes}</option>
+              </select>
+            </div>
+
+            <h1>Daily Planner</h1>
+
+            <p className="date">{date.toLocaleString()}</p>
+            <div className="dropdown-menu">
+              <FontAwesomeIcon icon="fa-solid fa-user" />
+              <div className="dropdown-content">
+                <p>Logged in as {currentUser.email}</p>
+                <button onClick={handleSignOut}>
+                  <FontAwesomeIcon icon="fa-solid fa-right-from-bracket" />
+                  <p>Sign Out</p>
+                </button>
+              </div>
+            </div>
+          </nav>
+          <div className="display-grid">
+            <Priorities />
+            <Reminders />
+            <ToDo />
+            <Notes />
           </div>
-        </nav>
-        <div className="display-grid">
-          <Priorities />
-          <Reminders />
-          <ToDo />
-          <Notes />
         </div>
-      </div>
+      </section>
     </>
   );
 };
